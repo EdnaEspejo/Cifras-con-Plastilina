cf2ad9d30813f038ff8933b43ca5e63a9943c4e5
EdnaEspejo <elespejo@poligran.edu.co>
2017-05-20 10:16:38 -0500

    Creación Base de Datos #5
    Modelamiento incial, pendiente definir modelo entidad relación

---
+++ Backend/Base Datos/Cifrasplastilina.sql
@@ -0,0 +1,26 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 20-05-2017 a las 00:13:30
+-- Versión del servidor: 10.1.21-MariaDB
+-- Versión de PHP: 7.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `cifrasplastilina`
+--
+CREATE DATABASE IF NOT EXISTS `cifrasplastilina`` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `pactohonor`;
+
+-- --------------------------------------------------------
+
